6b60cdf7991217df74c702a38634978bc3d679a3
Aidyn Altay <aidyn.altay@nu.edu.kz>
2023-02-20 19:54:31 +0600

    add jest, first test for classNames function

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,22 @@
+import { classNames } from './classNames'
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('class')).toBe('class')
+    })
+
+    test('with additional params', () => {
+        const expected = 'class class1 class2'
+        expect(classNames('class', {}, ['class1', 'class2'])).toBe(expected)
+    })
+
+    test('with mods', () => {
+        const expected = 'class class1 class2 hovered scrollable'
+        expect(classNames('class', { hovered: true, scrollable: true }, ['class1', 'class2'])).toBe(expected)
+    })
+
+    test('with one mod false', () => {
+        const expected = 'class class1 class2 scrollable'
+        expect(classNames('class', { hovered: false, scrollable: true }, ['class1', 'class2'])).toBe(expected)
+    })
+})
