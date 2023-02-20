6cc5d6f7de5577c8fca4e78579f58078a66dcae6
Aidyn Altay <aidyn.altay@nu.edu.kz>
2023-02-20 18:59:06 +0600

    stylelint configuration installed, i18next plugin installed

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,14 @@ module.exports = {
     es2021: true,
     jest: true
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:i18next/recommended'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json'
   },
-  plugins: ['react'],
+  plugins: ['react', 'i18next'],
   rules: {
     'react/jsx-indent': [2, 4],
     'react/jsx-indent-props': [2, 4],
@@ -37,7 +37,8 @@ module.exports = {
     'react/no-unescaped-entities': 'off',
     '@typescript-eslint/no-floating-promises': 'warn',
     'no-unused-vars': 'warn',
-    '@typescript-eslint/naming-convention': 'off'
+    '@typescript-eslint/naming-convention': 'off',
+    'i18next/no-literal-string': ['error', { markupOnly: true }]
   },
   globals: {
     '__IS_DEV__': true,
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,7 +2,6 @@ import { type FC } from 'react'
 import { Button, ThemeButton } from 'shared/ui/Button/Button'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Theme, useTheme } from 'app/providers/ThemeProvider'
-import cls from './ThemeSwitcher.module.scss'
 import DarkIcon from 'shared/assets/icons/theme-dark.svg'
 import LightIcon from 'shared/assets/icons/theme-light.svg'
 
@@ -18,7 +17,7 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = (props) => {
         <Button
             theme={ThemeButton.CLEAR}
             onClick={toggleTheme}
-            className={classNames(cls.themeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
         >
             {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
         </Button>
